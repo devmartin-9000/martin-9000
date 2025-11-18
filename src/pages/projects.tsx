9cbde7c3f3cd630d3172fc3eeecd75b7d8e4af6e
magonazz1 <martinmagonazz@outlook.com>
2025-11-18 06:40:59 +0200

    add: github button in project section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -179,6 +179,30 @@ export default function Projects() {
 					</div>
 				</div>
 			</div>
+			<div>
+				<div className="max-w-3xl mx-auto justify-center flex mt-4 rounded-4xl bg-primary p-4">
+					<h1 className="text-center text-sm md:text-base font-bold flex flex-row gap-4 items-center text-muted">
+						More projects? Visit GitHub
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<div className="flex items-center gap-2 p-2 border rounded-4xl hover:scale-105 animate-bounce hover:animate-none duration-500 transition-all">
+									<a target="_blank" href="https://github.com/devmartin-9000"
+									>
+										<img
+											className="h-8 w-auto"
+											src="github-icon.svg"
+											alt="Github Icon"
+										/>
+									</a>
+								</div>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p className="text-xs">Opens in a new tab</p>
+							</TooltipContent>
+						</Tooltip>
+					</h1>
+				</div>
+			</div>
 		</section>
 	);
 }
