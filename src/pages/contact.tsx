d7de449bd9a03a21da2aa111411a8638c5729896
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-04 03:15:11 +0200

    refine contact section

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,35 @@
-import { MessageSquareText, SendHorizontal  } from "lucide-react";
 import React from "react";
 
 const ContactSection: React.FC = () => {
+
+	interface ContactOptions {
+		name: string;
+		imageUrl: string;
+		link: string;
+	};
+
+	const contactItems: ContactOptions[] = [
+			{
+				name: "Email",
+				imageUrl: "./assets/email-icon.svg",
+				link: "mailto:dev.martinjack9000@gmail.com",
+			},
+			{
+				name: "Discord",
+				imageUrl: "./assets/discord-icon.svg",
+				link: "https://discord.com/devmartin-9000",
+			},
+			{
+				name: "LinkedIn",
+				imageUrl: "./assets/linkedin-icon.svg",
+				link: "https://linkedin.com/in/martinjackmagonagona",
+			},
+			{
+				name: "X (Twitter)",
+				imageUrl: "./assets/x-icon.png",
+				link: "https://x.com/devmartin9000",
+			},
+		];
 	return (
 		<section
 			id="contact"
@@ -9,7 +37,7 @@ const ContactSection: React.FC = () => {
 		>
 			<div className="border border-background relative w-95 sm:w-230 md:w-190 lg:w-230 py-10 mt-10 lg:mt-12 max-w-5xl h-full overflow-hidden rounded-[40px] bg-black dark:bg-white/50 p-6 md:p-16">
 				{/* Background circles */}
-				<div className="absolute inset-0 hidden h-full w-full overflow-hidden md:block">
+				<div className="absolute inset-0 h-full w-full overflow-hidden md:block">
 					<div className="absolute top-1/2 right-[-45%] aspect-square h-[900px] w-[900px] -translate-y-1/2">
 						<div className="absolute inset-0 rounded-full bg-primary/30 opacity-30"></div>
 						<div className="absolute inset-0 scale-[0.8] rounded-full bg-accent/40 opacity-30"></div>
@@ -22,30 +50,33 @@ const ContactSection: React.FC = () => {
 
 				{/* Content */}
 				<div className="relative z-10 flex flex-col items-center text-center gap-4">
-					<h1 className="text-5xl md:text-6xl font-bold text-muted">
+					<h1 className="text-4xl md:text-6xl font-bold text-muted">
 						Let's Get In Touch.
 					</h1>
-					<p className="max-w-2xl text-lg sm:text-xl md:text-2xl text-muted">
+					<p className="max-w-2xl text-md sm:text-xl md:text-2xl text-muted">
 						Let's connect and discuss how I can bring value to your team. I'm
 						just a message away.
 					</p>
 
-					<div className="flex flex-col sm:flex-row gap-2 md:gap-6 md:mt-6">
-						<a target="_blank" href="mailto:dev.martinjack9000@gmail.com">
-							<div className="bg-primary backdrop-blur-2xl flex gap-3 py-2 md:py-2 px-6 rounded-full hover:translate-y-2 transform transition-all duration-400 items-center text-center text-black font-bold">
-								Send Email
-								<SendHorizontal />
-							</div>
-						</a>
-						<a
-							target="_blank"
-							href="https://wa.me/265995571632?text=Hello%20Martin"
-						>
-							<div className="bg-primary backdrop-blur-2xl flex gap-3 py-2 md:py-2 px-6 rounded-full hover:translate-y-2 transform transition-all duration-400 items-center text-center text-black font-bold">
-								WhatsApp
-								<MessageSquareText />
-							</div>
-						</a>
+					<div className="flex flex-wrap sm:flex-row gap-2 md:gap-6 md:mt-6">
+						<div className="flex p-2 gap-1">
+							{contactItems.map((item) => (
+								<a
+									key={item.link}
+									href={item.link}
+									target="_blank"
+									rel="noreferrer"
+									className="p-2 flex flex-col items-center text-muted gap-2 "
+								>
+									<img
+										src={item.imageUrl}
+										alt={`${item.name} icon`}
+										className="w-auto h-12 p-2 rounded-md bg-primary "
+									/>
+									<span className="text-xs">{item.name}</span>
+								</a>
+							))}
+						</div>
 					</div>
 				</div>
 			</div>
